18b6fea0e95ec7a6967bf5bc3fcb3937e3084cc2
Ville Fokke Saarivaara <ville.saarivaara@gmail.com>
2023-03-23 23:17:15 +0200

    Build

--- dist/Bartender.mjs
+++ dist/Bartender.mjs
@@ -1,57 +1,79 @@
 var E = Object.defineProperty;
-var m = Object.getOwnPropertySymbols;
-var p = Object.prototype.hasOwnProperty, v = Object.prototype.propertyIsEnumerable;
-var u = (i, e, t) => e in i ? E(i, e, { enumerable: !0, configurable: !0, writable: !0, value: t }) : i[e] = t, h = (i, e) => {
+var f = Object.getOwnPropertySymbols;
+var w = Object.prototype.hasOwnProperty, g = Object.prototype.propertyIsEnumerable;
+var b = (r, e, t) => e in r ? E(r, e, { enumerable: !0, configurable: !0, writable: !0, value: t }) : r[e] = t, a = (r, e) => {
   for (var t in e || (e = {}))
-    p.call(e, t) && u(i, t, e[t]);
-  if (m)
-    for (var t of m(e))
-      v.call(e, t) && u(i, t, e[t]);
-  return i;
+    w.call(e, t) && b(r, t, e[t]);
+  if (f)
+    for (var t of f(e))
+      g.call(e, t) && b(r, t, e[t]);
+  return r;
 };
-var r = (i, e, t) => (u(i, typeof e != "symbol" ? e + "" : e, t), t);
-var f = (i, e, t) => new Promise((s, n) => {
-  var o = (a) => {
+var n = (r, e, t) => (b(r, typeof e != "symbol" ? e + "" : e, t), t);
+var l = (r, e, t) => new Promise((s, i) => {
+  var h = (o) => {
     try {
-      l(t.next(a));
-    } catch (b) {
-      n(b);
+      d(t.next(o));
+    } catch (c) {
+      i(c);
     }
-  }, d = (a) => {
+  }, p = (o) => {
     try {
-      l(t.throw(a));
-    } catch (b) {
-      n(b);
+      d(t.throw(o));
+    } catch (c) {
+      i(c);
     }
-  }, l = (a) => a.done ? s(a.value) : Promise.resolve(a.value).then(o, d);
-  l((t = t.apply(i, e)).next());
+  }, d = (o) => o.done ? s(o.value) : Promise.resolve(o.value).then(h, p);
+  d((t = t.apply(r, e)).next());
 });
-const c = (i, e) => {
-  if (i instanceof HTMLElement)
-    return i;
+const u = (r, e) => {
+  if (r instanceof HTMLElement)
+    return r;
   if (typeof e == "string")
     return document.querySelector(e) || void 0;
-}, w = (i = 100) => new Promise((e) => i ? setTimeout(e, i) : e());
-class _ {
-  constructor(e, t = {}, s) {
-    r(this, "bartender");
-    r(this, "name");
-    r(this, "el");
-    r(this, "elSelector");
-    r(this, "position");
-    r(this, "mode");
-    r(this, "isOpen", !1);
-    r(this, "openTimeout");
+}, m = (r = 100) => new Promise((e) => r ? setTimeout(e, r) : e());
+class v {
+  constructor(e, t = {}) {
+    n(this, "name");
+    n(this, "el");
+    n(this, "elSelector");
+    n(this, "position");
+    n(this, "mode");
+    n(this, "isPushing", !1);
+    n(this, "permanent", !1);
+    n(this, "isOpen", !1);
     if (!e)
       throw "Bar name is required";
-    if (!s)
-      throw `You must pass Bartender instance for bar '${this.name}'`;
-    if (Object.assign(this, t), this.name = e, this.bartender = s, this.el = c(this.el, this.elSelector), !this.el)
+    if (Object.assign(this, t), this.name = e, this.el = u(this.el, this.elSelector), !this.el)
       throw `Content element for bar '${this.name}' is required`;
-    if (this.el.parentElement !== this.bartender.mainEl)
-      throw `Element of bar '${this.name}' must be a direct child of the Bartender main element`;
-    this.el.classList.add("bartender__bar"), this.setPosition(this.position), this.setMode(this.mode);
+    this.el.style.transition = "none", this.el.classList.add("bartender__bar"), this.setPosition(this.position), this.setMode(this.mode), setTimeout(() => {
+      this.el && (this.el.style.transition = "");
+    });
   }
+  getTransitionDuration() {
+    if (!this.el)
+      return 0;
+    const e = window.getComputedStyle(this.el).getPropertyValue("transition-duration") || "0s";
+    return parseFloat(e) * 1e3;
+  }
+  // TODO: fix this. Matrix values are incorrect when closing
+  // private hasTransition () : boolean {
+  //   if (
+  //     !this.el ||
+  //     !window.getComputedStyle(this.el).getPropertyValue('transition-duration') ||
+  //     window.getComputedStyle(this.el).getPropertyValue('transition-duration') == '0s' ||
+  //     !window.getComputedStyle(this.el).getPropertyValue('transition-timing-function')
+  //   ) return false
+  //   const matrix = window.getComputedStyle(this.el).getPropertyValue('transform')
+  //   if (!matrix) return false
+  //   const parsedMatrix = matrix.match(/matrix.*\((.+)\)/)
+  //   if (!parsedMatrix || !parsedMatrix.length) return false
+  //   const matrixValues = parsedMatrix[1].split(', ')
+  //   if (!matrixValues.length) return false
+  //   console.log(matrixValues)
+  //   if (matrixValues[4] === '0' && matrixValues[5] === '0') return false
+  //   return true
+  // }
   /**
    * Set position
    *
@@ -60,7 +82,7 @@ class _ {
    * @returns this
    */
   setPosition(e) {
-    var s, n;
+    var s, i;
     if (!e)
       throw `Position is required for bar '${this.name}'`;
     const t = [
@@ -71,7 +93,7 @@ class _ {
     ];
     if (!t.includes(e))
       throw `Invalid position '${e}' for bar '${this.name}'. Use one of the following: ${t.join(", ")}.`;
-    return this.position && ((s = this.el) == null || s.classList.remove(`bartender__bar--${this.position}`)), this.position = e, (n = this.el) == null || n.classList.add(`bartender__bar--${this.position}`), this;
+    return this.position && ((s = this.el) == null || s.classList.remove(`bartender__bar--${this.position}`)), this.position = e, (i = this.el) == null || i.classList.add(`bartender__bar--${this.position}`), this;
   }
   /**
    * Set mode
@@ -81,7 +103,7 @@ class _ {
    * @returns this
    */
   setMode(e) {
-    var s, n;
+    var s, i;
     if (!e)
       throw `Mode is required for bar '${this.name}'`;
     const t = [
@@ -91,64 +113,68 @@ class _ {
     ];
     if (!t.includes(e))
       throw `Invalid mode '${e}' for bar '${this.name}'. Use one of the following: ${t.join(", ")}.`;
-    return this.mode && ((s = this.el) == null || s.classList.remove(`bartender__bar--${this.mode}`)), this.mode = e, (n = this.el) == null || n.classList.add(`bartender__bar--${this.mode}`), this;
+    return this.isPushing = ["push", "reveal"].includes(e), console.log(this.isPushing), this.mode && ((s = this.el) == null || s.classList.remove(`bartender__bar--${this.mode}`)), this.mode = e, (i = this.el) == null || i.classList.add(`bartender__bar--${this.mode}`), this;
   }
   open() {
-    return f(this, null, function* () {
-      var t, s;
-      const e = (t = this.bartender) == null ? void 0 : t.getOpenBar();
-      return (e == null ? void 0 : e.name) === this.name ? Promise.resolve(this) : ((s = this.bartender) == null ? void 0 : s.busy) === !0 ? Promise.resolve(this) : (this.bartender.busy = !0, e && (yield e.close(), yield w(this.openTimeout)), new Promise((n) => {
-        var o, d, l;
-        (o = this.el) == null || o.addEventListener("transitionend", () => {
-          var a;
-          return (a = this.el) == null || a.dispatchEvent(new CustomEvent("bartender-bar-after-open", {
-            bubbles: !0,
-            detail: {
-              bar: this
-            }
-          })), this.bartender.busy = !1, n(this);
-        }, {
-          once: !0
-        }), (d = this.el) == null || d.dispatchEvent(new CustomEvent("bartender-bar-before-open", {
-          bubbles: !0,
-          detail: {
-            bar: this
-          }
-        })), (l = this.el) == null || l.classList.add("bartender__bar--open"), this.isOpen = !0;
-      }));
-    });
-  }
-  close() {
-    return new Promise((e) => {
-      var t, s, n;
-      (t = this.el) == null || t.addEventListener("transitionend", () => {
-        var o;
-        return (o = this.el) == null || o.dispatchEvent(new CustomEvent("bartender-bar-after-close", {
-          bubbles: !0,
-          detail: {
-            bar: this
-          }
-        })), e(this);
-      }, {
-        once: !0
-      }), (s = this.el) == null || s.dispatchEvent(new CustomEvent("bartender-bar-before-close", {
+    return l(this, null, function* () {
+      var e, t, s;
+      return (e = this.el) == null || e.dispatchEvent(new CustomEvent("bartender-bar-before-open", {
         bubbles: !0,
         detail: {
           bar: this
         }
-      })), (n = this.el) == null || n.classList.remove("bartender__bar--open"), this.isOpen = !1;
+      })), (t = this.el) == null || t.classList.add("bartender__bar--open"), this.isOpen = !0, yield m(this.getTransitionDuration()), (s = this.el) == null || s.dispatchEvent(new CustomEvent("bartender-bar-after-open", {
+        bubbles: !0,
+        detail: {
+          bar: this
+        }
+      })), Promise.resolve(this);
     });
   }
-  toggle() {
-    return this.isOpen === !0 ? this.close() : this.open();
+  close() {
+    return l(this, null, function* () {
+      var e, t, s;
+      return (e = this.el) == null || e.dispatchEvent(new CustomEvent("bartender-bar-before-close", {
+        bubbles: !0,
+        detail: {
+          bar: this
+        }
+      })), (t = this.el) == null || t.classList.remove("bartender__bar--open"), this.isOpen = !1, yield m(this.getTransitionDuration()), (s = this.el) == null || s.dispatchEvent(new CustomEvent("bartender-bar-after-close", {
+        bubbles: !0,
+        detail: {
+          bar: this
+        }
+      })), Promise.resolve(this);
+    });
+  }
+  getPushStyles() {
+    return !this.position || !this.el ? null : {
+      transform: (() => {
+        var e, t, s, i;
+        switch (this.position) {
+          case "left":
+            return `translateX(${(e = this.el) == null ? void 0 : e.offsetWidth}px)`;
+          case "right":
+            return `translateX(-${(t = this.el) == null ? void 0 : t.offsetWidth}px)`;
+          case "top":
+            return `translateY(${(s = this.el) == null ? void 0 : s.offsetHeight}px)`;
+          case "bottom":
+            return `translateY(-${(i = this.el) == null ? void 0 : i.offsetHeight}px)`;
+          default:
+            return "";
+        }
+      })(),
+      transitionDuration: window.getComputedStyle(this.el).getPropertyValue("transition-duration") || "",
+      transitionTimingFunction: window.getComputedStyle(this.el).getPropertyValue("transition-timing-function") || ""
+    };
   }
 }
-class g {
+class P {
   /**
    * Constructor
    *
-   * @param options Bartender options
-   * @param barDefaultOptions Default options for new bars
+   * @param userOptions Bartender options
+   * @param barOptions Default options for new bars
    * @throws Error message
    */
   constructor(e = {}, t = {}) {
@@ -157,57 +183,51 @@ class g {
     // TODO: closeOnEsc should be defined per bar
     // TODO: trapFocus should be defined per bar
     // TODO: scrollTop should be defined per bar
-    r(this, "debug", !1);
-    r(this, "busy", !1);
-    r(this, "mainEl");
-    r(this, "mainElSelector");
-    r(this, "contentEl");
-    r(this, "contentElSelector");
-    r(this, "bars", {});
-    r(this, "barDefaultOptions", {});
-    if (Object.assign(this, h({
+    n(this, "debug", !1);
+    n(this, "busy", !1);
+    n(this, "mainEl");
+    n(this, "mainElSelector");
+    n(this, "contentEl");
+    n(this, "contentElSelector");
+    n(this, "switchTimeout");
+    n(this, "bars", []);
+    n(this, "pushableElements", []);
+    n(this, "barOptions", {});
+    Object.assign(this, a(a({}, {
       debug: !1,
       mainEl: void 0,
       mainElSelector: ".bartender",
       contentEl: void 0,
-      contentElSelector: ".bartender__content"
-    }, e)), Object.assign(this.barDefaultOptions, h({
+      contentElSelector: ".bartender__content",
+      switchTimeout: 100
+    }), e));
+    const i = {
       position: "left",
       mode: "float",
-      openTimeout: 100
-    }, t)), this.mainEl = c(this.mainEl, this.mainElSelector), !this.mainEl)
+      permanent: !1
+    };
+    if (this.barOptions = a(a({}, i), t), this.mainEl = u(this.mainEl, this.mainElSelector), !this.mainEl)
       throw "Main element is required";
-    if (this.contentEl = c(this.contentEl, this.contentElSelector), !this.contentEl)
+    if (this.contentEl = u(this.contentEl, this.contentElSelector), !this.contentEl)
       throw "Content element is required";
-    if (this.contentEl.parentElement !== this.mainEl)
+    if (this.addPushElement({
+      el: this.contentEl
+    }), this.contentEl.parentElement !== this.mainEl)
       throw "Content element must be a direct child of the main element";
-    this.mainEl.classList.add("bartender--ready"), this.mainEl.dispatchEvent(new CustomEvent("bartender-init", {
+    window.addEventListener("keydown", this.handleKeydown.bind(this)), this.mainEl.classList.add("bartender--ready"), this.mainEl.dispatchEvent(new CustomEvent("bartender-init", {
       bubbles: !0,
       detail: {
         bartender: this
       }
     }));
   }
-  /**
-   * Add a new bar
-   *
-   * @param name Unique name for the bar
-   * @param options Bar options
-   * @throws Error message
-   * @returns this
-   */
-  addBar(e, t = {}) {
-    var s;
-    if (!e || typeof e != "string")
-      throw "Name is required";
-    if (this.bars[e])
-      throw `Bar with name '${e}' is already defined`;
-    return t = Object.assign({}, h(h({}, this.barDefaultOptions), t)), this.bars[e] = new _(e, t, this), (s = this.mainEl) == null || s.dispatchEvent(new CustomEvent("bartender-bar-added", {
-      bubbles: !0,
-      detail: {
-        bar: this.bars[e]
-      }
-    })), this;
+  // TODO: make sure this is updated
+  destroy() {
+    var e;
+    (e = this.mainEl) == null || e.classList.remove("bartender--ready");
+  }
+  getBar(e) {
+    return this.bars.find((t) => t.name === e) || null;
   }
   /**
    * Get currently open bar
@@ -215,10 +235,73 @@ class g {
    * @returns Bar object
    */
   getOpenBar() {
-    const e = Object.keys(this.bars).find((t) => this.bars[t].isOpen === !0);
-    return e ? this.bars[e] : null;
+    return this.bars.find((e) => e.isOpen === !0) || null;
+  }
+  /**
+   * Add a new bar
+   *
+   * @param name Unique name for the bar
+   * @param userOptions Bar options
+   * @throws Error message
+   * @returns this
+   */
+  addBar(e, t = {}) {
+    var h;
+    if (!e || typeof e != "string")
+      return Promise.reject(new Error("Name is required"));
+    if (this.getBar(e))
+      return Promise.reject(new Error(`Bar with name '${e}' is already defined`));
+    const s = a(a({}, this.barOptions), t), i = new v(e, s);
+    return this.bars.push(i), (h = this.mainEl) == null || h.dispatchEvent(new CustomEvent("bartender-bar-added", {
+      bubbles: !0,
+      detail: {
+        bar: i
+      }
+    })), Promise.resolve(i);
+  }
+  open(e) {
+    return l(this, null, function* () {
+      var i;
+      const t = this.getBar(e);
+      return t ? t.isOpen === !0 ? Promise.reject(new Error(`Bar '${t.name}' is already open`)) : this.busy === !0 ? Promise.reject(new Error("Bartender is busy")) : (this.busy = !0, this.getOpenBar() && (yield this.close(), yield m(this.switchTimeout)), (i = this.mainEl) == null || i.classList.add("bartender--open"), t.isPushing === !0 && this.pushElements(t.getPushStyles()), yield t.open(), this.busy = !1, Promise.resolve(t)) : Promise.reject(new Error(`Unknown bar '${e}'`));
+    });
+  }
+  close() {
+    return l(this, null, function* () {
+      var t;
+      const e = this.getOpenBar();
+      return e ? (this.pullElements(), yield e.close(), (t = this.mainEl) == null || t.classList.remove("bartender--open"), Promise.resolve(e)) : Promise.resolve(null);
+    });
+  }
+  toggle(e) {
+    return l(this, null, function* () {
+      const t = this.getBar(e);
+      return t ? t.isOpen === !0 ? this.close() : this.open(e) : Promise.reject(new Error(`Unknown bar '${e}'`));
+    });
+  }
+  // TODO: support push elements per bar
+  addPushElement(e = {}) {
+    const t = u(e.el, e.elSelector);
+    return t ? (this.pushableElements.push(t), Promise.resolve(t)) : Promise.reject(new Error("Unknown push element"));
+  }
+  pushElements(e) {
+    if (!e || !this.pushableElements.length)
+      return null;
+    for (const t of this.pushableElements)
+      t.style.transform = e.transform, t.style.transitionDuration = e.transitionDuration, t.style.transitionTimingFunction = e.transitionTimingFunction;
+    return this.pushableElements;
+  }
+  pullElements() {
+    if (!this.pushableElements.length)
+      return null;
+    for (const e of this.pushableElements)
+      e.style.transform = "translateX(0) translateY(0)";
+    return this.pushableElements;
+  }
+  handleKeydown(e) {
+    e.key === "Escape" && this.close();
   }
 }
 export {
-  g as Bartender
+  P as Bartender
 };
