5a69e5e48699d3727f081686bac5577e5336e4a5
Ville Fokke Saarivaara <ville.saarivaara@gmail.com>
2024-02-29 11:32:01 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
 import './assets/bartender.scss'
 import { Bartender } from './lib/Bartender'
 
-// import {
-//   BartenderOptions,
-//   BartenderBarDefaultOptions,
-//   BartenderBarOptions,
-//   BartenderElementQuery,
-//   BartenderPushElementOptions
-// } from './types'
-
-// export type {
-//   BartenderOptions
-// } from './types'
-
 export type * from './lib/types'
 
 export {
