ab75bb428266bcbedd9a9e5d70688148583942e1
Ville Fokke Saarivaara <ville.saarivaara@gmail.com>
2025-06-05 23:58:44 +0300

    Export events

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@ export type * from './lib/types'
 import './assets/bartender.scss'
 export { Bartender } from './lib/Bartender'
 export { BartenderBar } from './lib/BartenderBar'
+export {
+  BartenderInstanceEvent,
+  BartenderBarEvent,
+  BartenderBarUpdatedEvent,
+  BartenderBarRemovedEvent,
+} from './lib/events'
+export type {
+  BartenderInstanceEventDetail,
+  BartenderBarEventDetail,
+  BartenderBarUpdatedEventDetail,
+  BartenderBarRemovedEventDetail,
+} from './lib/events'
